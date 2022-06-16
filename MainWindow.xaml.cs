6b2ce685a28dfbe8b798d0525526959aa2b52a31
TomaszCiuk <tomasz.ciuk@microsoft.wsei.edu.pl>
2022-06-16 23:52:44 +0200

    Dodano odczytywanie ceny przedmiotów dla handlarzy

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Programowanie_Obiektowe___Projekt
 {
@@ -20,6 +12,8 @@ namespace Programowanie_Obiektowe___Projekt
     /// </summary>
     public partial class MainWindow : Window
     {
+        private EditingWindow wnd;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,11 +22,11 @@ namespace Programowanie_Obiektowe___Projekt
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
 
-            Programowanie_Obiektowe___Projekt.PROJEKTDataSet pROJEKTDataSet = ((Programowanie_Obiektowe___Projekt.PROJEKTDataSet)(this.FindResource("pROJEKTDataSet")));
+            PROJEKTDataSet pROJEKTDataSet = (PROJEKTDataSet)this.FindResource("pROJEKTDataSet");
             // Załaduj dane do tabeli Itemy. Możesz modyfikować ten kod w razie potrzeby.
-            Programowanie_Obiektowe___Projekt.PROJEKTDataSetTableAdapters.ItemyTableAdapter pROJEKTDataSetItemyTableAdapter = new Programowanie_Obiektowe___Projekt.PROJEKTDataSetTableAdapters.ItemyTableAdapter();
+            PROJEKTDataSetTableAdapters.ItemyTableAdapter pROJEKTDataSetItemyTableAdapter = new PROJEKTDataSetTableAdapters.ItemyTableAdapter();
             pROJEKTDataSetItemyTableAdapter.Fill(pROJEKTDataSet.Itemy);
-            System.Windows.Data.CollectionViewSource itemyViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("itemyViewSource")));
+            System.Windows.Data.CollectionViewSource itemyViewSource = (System.Windows.Data.CollectionViewSource)this.FindResource("itemyViewSource");
             itemyViewSource.View.MoveCurrentToFirst();
 
             var queryBarterItemsDataGrid =
@@ -113,8 +107,13 @@ namespace Programowanie_Obiektowe___Projekt
             mapsEquipmentDataGrid.ItemsSource = queryMapsDataGrid.ToList();
             moneyEquipmentDataGrid.ItemsSource = queryMoneyDataGrid.ToList();
 
-
+            // Załaduj dane do tabeli Prapor. Możesz modyfikować ten kod w razie potrzeby.
+            PROJEKTDataSetTableAdapters.PraporTableAdapter pROJEKTDataSetPraporTableAdapter = new PROJEKTDataSetTableAdapters.PraporTableAdapter();
+            pROJEKTDataSetPraporTableAdapter.Fill(pROJEKTDataSet.Prapor);
+            System.Windows.Data.CollectionViewSource praporViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("praporViewSource")));
+            praporViewSource.View.MoveCurrentToFirst();
         }
+
 
         private void Button_AssingBarterItemsClick(object sender, RoutedEventArgs e)
         {
@@ -226,6 +225,112 @@ namespace Programowanie_Obiektowe___Projekt
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (wnd != null || wnd.IsVisible) return;
+            wnd = new EditingWindow();
+            wnd.Show();
+        }
+
+        private void praporPrice_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ButtonPrice_Click(object sender, RoutedEventArgs e)
+        {
+            PROJEKTDataSet pROJEKTDataSet = (PROJEKTDataSet)FindResource("pROJEKTDataSet");
+            // Załaduj dane do tabeli Itemy. Możesz modyfikować ten kod w razie potrzeby.
+            PROJEKTDataSetTableAdapters.ItemyTableAdapter pROJEKTDataSetItemyTableAdapter = new PROJEKTDataSetTableAdapters.ItemyTableAdapter();
+            pROJEKTDataSetItemyTableAdapter.Fill(pROJEKTDataSet.Itemy);
+            CollectionViewSource itemyViewSource = (CollectionViewSource)FindResource("itemyViewSource");
+            itemyViewSource.View.MoveCurrentToFirst();
+
+            var quaryPrapor =
+                from prapor in pROJEKTDataSet.Prapor
+                join item in pROJEKTDataSet.Itemy
+                on prapor.ItemID equals item.ID
+                where item.Nazwa == NazwaItemu.Text
+                select prapor.CenaKupna;
+
+                praporPrice.Text =
+                    quaryPrapor.FirstOrDefault() == 0
+                    ? "Handlarz nie kupuje przedmiotu"
+                    : $"{quaryPrapor.FirstOrDefault()} ₽";
+
+            var quaryTherapist =
+                from therapist in pROJEKTDataSet.Therapist
+                join item in pROJEKTDataSet.Itemy
+                on therapist.ItemID equals item.ID
+                where item.Nazwa == NazwaItemu.Text
+                select therapist.CenaKupna;
+
+            therapistPrice.Text =
+                quaryTherapist.FirstOrDefault() == 0
+                ? "Handlarz nie kupuje przedmiotu"
+                : $"{quaryTherapist.FirstOrDefault()} ₽";
+
+            var quarySkier =
+                from skier in pROJEKTDataSet.Skier
+                join item in pROJEKTDataSet.Itemy
+                on skier.ItemID equals item.ID
+                where item.Nazwa == NazwaItemu.Text
+                select skier.CenaKupna;
+
+            skierPrice.Text =
+                quarySkier.FirstOrDefault() == 0
+                ? "Handlarz nie kupuje przedmiotu"
+                : $"{quarySkier.FirstOrDefault()} ₽";
+
+            var quaryPeacekeeper =
+                from peacekeeper in pROJEKTDataSet.Peacekeeper
+                join item in pROJEKTDataSet.Itemy
+                on peacekeeper.ItemID equals item.ID
+                where item.Nazwa == NazwaItemu.Text
+                select peacekeeper.CenaKupna;
+
+            peacekeeperPrice.Text =
+                quaryPeacekeeper.FirstOrDefault() == 0
+                ? "Handlarz nie kupuje przedmiotu"
+                : $"{quaryPeacekeeper.FirstOrDefault()} $";
+
+            var quaryMechanic =
+                from mechanic in pROJEKTDataSet.Mechanic
+                join item in pROJEKTDataSet.Itemy
+                on mechanic.ItemID equals item.ID
+                where item.Nazwa == NazwaItemu.Text
+                select mechanic.CenaKupna;
+
+            mechanicPrice.Text =
+                quaryMechanic.FirstOrDefault() == 0
+                ? "Handlarz nie kupuje przedmiotu"
+                : $"{quaryMechanic.FirstOrDefault()} ₽";
+
+            var quaryRagman =
+                from ragman in pROJEKTDataSet.Ragman
+                join item in pROJEKTDataSet.Itemy
+                on ragman.ItemID equals item.ID
+                where item.Nazwa == NazwaItemu.Text
+                select ragman.CenaKupna;
+
+            ragmanPrice.Text =
+                quaryRagman.FirstOrDefault() == 0
+                ? "Handlarz nie kupuje przedmiotu"
+                : $"{quaryRagman.FirstOrDefault()} ₽";
+
+            var quaryJaeger =
+                from jaeger in pROJEKTDataSet.Jaeger
+                join item in pROJEKTDataSet.Itemy
+                on jaeger.ItemID equals item.ID
+                where item.Nazwa == NazwaItemu.Text
+                select jaeger.CenaKupna;
+
+            jaegerPrice.Text =
+                quaryJaeger.FirstOrDefault() == 0
+                ? "Handlarz nie kupuje przedmiotu"
+                : $"{quaryJaeger.FirstOrDefault()} ₽";
         }
     }
 }
